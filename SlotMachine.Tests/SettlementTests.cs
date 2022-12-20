e68d2de98a4d8c1789814a5503f6a5802aa10e8c
angel.stoykov <angel.stoykov@intradiem.com>
2022-12-21 00:19:07 +0200

    added settlement tests
    Change-Id: I42bc8133923fc9d72cd9d16b30f05af0a58655cf

--- SlotMachine.Tests/SettlementTests.cs
+++ SlotMachine.Tests/SettlementTests.cs
@@ -1,4 +1,5 @@
 ﻿using SlotMachine.Core;
+using SlotMachine.Models.PrizeItems;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,42 @@ namespace SlotMachine.Tests
 {
     internal class SettlementTests
     {
+        [Test]
+        public void CalculateProfitTest()
+        {
+            const decimal bet = 100m;
+            const decimal expectedWinningCoefficientSameLetters = 5.4m;
+            const decimal expectedWinningCoefficientWithAsterix = 4.6m;
+
+            var actualWinningLinesWithSameLetters = GetWinningLinesSameLetters();
+            var actualWinningLinesWithAsterix = GetWinningLinesWithAsterix();
+
+            PrizeItemBase[] prizeItems = PrizeGenerator.GeneratePrizeItems();
+
+            var profitSameLetters = Settlement.CalculateProfit(bet, actualWinningLinesWithSameLetters, prizeItems);
+            var profitWithAsterix = Settlement.CalculateProfit(bet, actualWinningLinesWithAsterix, prizeItems);
+
+            Assert.That(Math.Round((bet * expectedWinningCoefficientSameLetters), 2), Is.EqualTo(profitSameLetters));
+            Assert.That(Math.Round((bet * expectedWinningCoefficientWithAsterix), 2), Is.EqualTo(profitWithAsterix));
+        }
+
+        [Test]
+        public void CalculateWinningCoefficientTest()
+        {
+            const decimal expectedWinningCoefficientSameLetters = 5.4m;
+            const decimal expectedWinningCoefficientWithAsterix = 4.6m;
+            PrizeItemBase[] prizeItems = PrizeGenerator.GeneratePrizeItems();
+
+            var actualWinningLines = GetWinningLinesSameLetters();
+            var actualWinningCoefficient = Settlement.CalculateProfitCoeficient(actualWinningLines, prizeItems);
+
+            var actualWinningLinesWithAsterix = GetWinningLinesWithAsterix();
+            var actualWinningCoefficientWithAsterix = Settlement.CalculateProfitCoeficient(actualWinningLinesWithAsterix, prizeItems);
+
+            Assert.That(actualWinningCoefficient, Is.EqualTo(expectedWinningCoefficientSameLetters));
+            Assert.That(actualWinningCoefficientWithAsterix, Is.EqualTo(expectedWinningCoefficientWithAsterix));
+        }
+
         [Test]
         public void SettlementEvaluateResultTest()
         {
